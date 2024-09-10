27bdfb43d3b2867cda8e57b6777b8b4267823f8f
Sebas <A01253494@tec.mx>
2024-09-10 12:50:37 -0600

    Fourth commit

---
+++ little-lemon/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import OnboardingScreen from './screens/Onboarding';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+    );
+}
